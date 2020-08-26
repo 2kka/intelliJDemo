f5808a3035d69e28a3fd2d51acb99907e7d91de9
Tuukka Niiranen <niiranen.tuukka@gmail.com>
2020-08-26 23:09:34 +0300

    ladataan gittiin

--- src/org/launchcode/Main.java
+++ src/org/launchcode/Main.java
@@ -3,7 +3,7 @@ package org.launchcode;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("ssfsf" +
+        System.out.println("MOI MAAILMA888" +
                 "");
     }
 }
